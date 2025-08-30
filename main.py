5f7a7a9dd098cf26e0810cd217c8dd0a60471f89
Ravi Kishan <kishanravi887321@gmail.com>
2025-08-30 13:43:26 +0530

    feat: add LlamaRead class for document indexing and querying with OpenAI API; include benefits, features, and introduction text files

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from voice_agent.stt.speech_to_text import SpeechToText
+# from voice_agent.stt.speech_to_text import SpeechToText
 
-stt = SpeechToText(api_key="3294cac533734554b16271ce993b897c")
-output = stt.start()
+# stt = SpeechToText(api_key="3294cac533734554b16271ce993b897c")
+# output = stt.start()
+
+
+from voice_agent.gather.llama_read import LlamaRead
+
+reader=LlamaRead(api_key="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",
+                 folder_path="data_folder")
+
